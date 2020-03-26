6d304031f60a7cd71c70bbf3925e8d9df943030b
Fernanda <femartinsoliveira@hotmail.com>
2020-03-26 14:11:28 -0300

    add pasta controllers / encapsulando rotas get && post

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,25 +1,8 @@
 const express = require("express");
-const crypto = require("crypto");
-const connection = require("./database/connection");
+const ongsController = require("./controllers/ongsController");
 const routes = express.Router();
 
-routes.get("/ongs", async (request, response) => {
-    const listarTodasAsOngs = await connection("ongs").select("*");
-    return response.json(listarTodasAsOngs);
-});
-
-routes.post("/ongs", async (request, response) => {
-    const { name, email, whatsapp, city, uf } = request.body;
-    const id = crypto.randomBytes(4).toString("HEX");
-    await connection("ongs").insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf
-    });
-    return response.json({ id });
-});
+routes.get("/ongs", ongsController.index);
+routes.post("/ongs",ongsController.create);
 
 module.exports = routes;
