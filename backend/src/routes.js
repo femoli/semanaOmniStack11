6dfb1f123142bb0f44bb6859cd9cc63f2be895d8
Fernanda <femartinsoliveira@hotmail.com>
2020-03-26 19:21:17 -0300

    add perfilOngsController && criando rota get: listarTodosOsIncidentesPorIdDaOng

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const ongsController = require("./controllers/ongsController");
 const incidentesController = require("./controllers/incidentesController");
+const perfilOngController = require("./controllers/perfilOngController");
 const routes = express.Router();
 
 //rotas ong
@@ -11,5 +12,8 @@ routes.post("/ongs", ongsController.create);
 routes.get("/incidentes", incidentesController.index);
 routes.post("/incidentes", incidentesController.create);
 routes.delete("/incidentes/:id", incidentesController.delete);
+
+//rotas perfil-ong
+routes.get("/perfil", perfilOngController.index);
 
 module.exports = routes;
